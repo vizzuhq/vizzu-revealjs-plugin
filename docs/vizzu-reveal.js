fc40f15d2de6ddb4f24296f951908000fd2d90b5
Laszlo Simon <simon@vizzuhq.com>
2023-06-14 15:47:07 +0200

    Support for multiple vizzu-players (on separate slides)

--- docs/vizzu-reveal.js
+++ docs/vizzu-reveal.js
@@ -5,6 +5,8 @@ let RevealVizzu = {
 	init: (deck) => initRevealVizzu(deck)
 }
 
+let players = [];
+
 const initRevealVizzu = (Reveal) => {
 
 	function _getInnerObj(element)
@@ -12,35 +14,51 @@ const initRevealVizzu = (Reveal) => {
 		return JSON.parse(element.innerText);
 	}
 
-	function initialize() {
-		const initDataElement = document.querySelectorAll("[data-vizzu-init-data]");
-		const initSlideElement = document.querySelectorAll("[data-vizzu-init-slide]");
-		const slideElements = [...document.querySelectorAll("[data-vizzu-slide]")];
+	function _initializeStory(storyElement)
+	{
+		const vp = storyElement.querySelector('vizzu-player');
+		players.push(vp);
+		const index = players.length - 1;
+
+		const initDataElement = storyElement.querySelectorAll("[data-vizzu-init-data]");
+		const initSlideElement = storyElement.querySelectorAll("[data-vizzu-init-slide]");
+		const slideElements = [...storyElement.querySelectorAll("[data-vizzu-slide]")];
 
 		const story = {
 			data: _getInnerObj(initDataElement[0]),
 			slides: [ _getInnerObj(initSlideElement[0]), ...slideElements.map(_getInnerObj) ]
 		};
 
-		const vp = document.querySelector('vizzu-player');
+		slideElements.map((slideElement) => { slideElement.dataset.VizzuPlayer = index; });
+
 		vp.slides = story;
 	}
 
-	const vp = document.querySelector('vizzu-player');
+	function _initialize() {
+		const storyElement = [...document.querySelectorAll("[data-vizzu]")];
+		storyElement.map(_initializeStory);
+	}
+
+	function _getInnerObj(element)
+	{
+		return JSON.parse(element.innerText);
+	}
 
 	return Reveal.addEventListener('ready', (event) => {
 
-		initialize();
+		_initialize();
 
 		Reveal.addEventListener( 'fragmentshown', function( event ) {
 			const actIndex = Number(event.fragment.dataset.fragmentIndex) + 1;
 			console.log('s',actIndex);
+			const vp = players[event.fragment.dataset.VizzuPlayer];
 			vp.currentSlide = actIndex;
 		} );
 
 		Reveal.addEventListener( 'fragmenthidden', function( event ) {
 			const actIndex = Number(event.fragment.dataset.fragmentIndex);
 			console.log('h',actIndex);
+			const vp = players[event.fragment.dataset.VizzuPlayer];
 			vp.currentSlide = actIndex;
 		} );
 
